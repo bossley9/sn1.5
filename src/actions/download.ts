af258044df0fe73124cb7e3a8cfacf9e7c01bddc
Sam Bossley <bossley.samuel@gmail.com>
2023-09-15 19:59:16 -0400

    feat: implement reset action

--- src/actions/download.ts
+++ src/actions/download.ts
@@ -4,15 +4,17 @@ import { authenticate } from "../client/auth.ts";
 import { initialSync } from "../client/initialSync.ts";
 
 export async function Download() {
-  logInfo("Downloading...");
   const client = await newClient();
   await authenticate(client);
 
+  logInfo("Downloading...");
   const changeVersion = client.storage.get<string>("cv") || "";
   if (changeVersion.length === 0) {
     logInfo("Change version not found. Making fresh sync...");
     await initialSync(client);
   }
+  // TODO update when change version exists
 
   client.simp.disconnect();
+  logInfo("Done.");
 }
--- src/actions/reset.ts
+++ src/actions/reset.ts
@@ -1,3 +1,13 @@
-export function Reset() {
-  console.log("TODO reset");
+import { logInfo } from "../logger.ts";
+import { newClient } from "../client/newClient.ts";
+import { authenticate } from "../client/auth.ts";
+import { initialSync } from "../client/initialSync.ts";
+
+export async function Reset() {
+  const client = await newClient();
+  await authenticate(client);
+  logInfo("Resetting...");
+  await initialSync(client);
+  client.simp.disconnect();
+  logInfo("Done.");
 }
